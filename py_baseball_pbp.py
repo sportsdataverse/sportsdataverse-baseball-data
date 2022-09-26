8683ad666115f4bd6f2f9095704a05d06a1d8dcb
Joseph Armstrong <70788681+armstjc@users.noreply.github.com>
2022-09-26 15:05:27 -0400

    Update py_baseball_pbp.py
    Signed-off-by: Joseph Armstrong <70788681+armstjc@users.noreply.github.com>

--- py_baseball_pbp.py
+++ py_baseball_pbp.py
@@ -6,7 +6,7 @@ from datetime import datetime
 if __name__ == '__main__':
     pybaseball.cache.enable()
     #for i in range(1920,datetime.now().year + 1):
-    for i in range(1972,datetime.now().year + 1):
+    for i in range(datetime.now().year,datetime.now().year + 1):
         print(i)
         season_df = statcast(start_dt=f'{i}-03-01', end_dt=f'{i}-12-01')
         # season = [game_year]
